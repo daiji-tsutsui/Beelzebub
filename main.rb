20443ea9fdc63fb6800d9f189f2efe6b75d7d9e2
Daiji TSUTSUI <daiji.tsutsui417@gmail.com>
2021-08-01 23:16:36 +0900

    Add help

--- main.rb
+++ main.rb
@@ -2,17 +2,38 @@ require 'uri'
 require 'net/http'
 require 'json'
 
-uri = URI.parse("https://versatileapi.herokuapp.com/api/text/all")
-params = {}
-uri.query = URI.encode_www_form(params)
-res = Net::HTTP.get_response(uri)
-hash = JSON.parse(res.body)
+def fetch
+  uri = URI.parse("https://versatileapi.herokuapp.com/api/text/all")
+  params = {}
+  uri.query = URI.encode_www_form(params)
+  res = Net::HTTP.get_response(uri)
+  return JSON.parse(res.body)
+end
+
+def help
+text = <<EOT
+  NUMBER: \tshow 5 tweets older than the latest by $NUMBER
+EOT
+puts text
+end
+
+
+
+hash = fetch
 
 while true
-  puts 'Input the index of post desired... (Press Ctrl+C to quit)'
-  num = gets.chomp.to_i
-  for i in 0..4
-    pp hash[num + i]["text"]
+  print "\n"
+  print 'Enter "help" or "\h" to display available commands...'
+  print "\n> "
+
+  command = gets.chomp
+
+  if /(\d+)/ === command
+    num = Regexp.last_match[0].to_i
+    for i in 0..4
+      pp hash[num + i]["text"]
+    end
+  elsif command == 'help' || command == '\h'
+    help
   end
-  puts "\n"
 end
